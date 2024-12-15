a681813a37316262dc29ec6698c6186d4c1f0fdf
Italo Espinoza <Italoespinoza1529@gmail.com>
2024-12-15 05:58:50 -0500

    add product and list

--- src/app/components/productos/productos.component.ts
+++ src/app/components/productos/productos.component.ts
@@ -1,11 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductosService } from '../../services/productos.service';
+import { Producto } from '../../interfaces/producto';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableDataSource } from '@angular/material/table'; 
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
+  standalone: true,
   selector: 'app-productos',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatPaginator, 
+    MatButtonModule, 
+    MatIconModule,
+    MatDialogModule
+  ],
   templateUrl: './productos.component.html',
   styleUrl: './productos.component.scss'
 })
-export class ProductosComponent {
+export class ProductosComponent implements OnInit, AfterViewInit {
+  productos: Producto[] = [];
+  displayedColumns: string[] = ['id', 'nombre', 'descripcion', 'precio', 'stock', 'acciones'];
+  dataSource: MatTableDataSource<Producto> = new MatTableDataSource<Producto>([]);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(
+    private productosService: ProductosService,
+    private router: Router,
+    private dialog: MatDialogModule
+  ) { }
+
+  ngOnInit(): void {
+    this.getProductos();
+  }
+
+  ngAfterViewInit(): void {
+    if(this.paginator){
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.paginator.firstPage(); 
+    }else {
+      //console.error("Paginator no está inicializado.");
+    }
+  }
+
+  getProductos(): void {
+    this.productosService.getProductos().subscribe(
+      (data) => {
+        //console.log("Productos obtenidos: ", data);
+        this.productos = data;
+        this.dataSource.data = this.productos;
+      },
+      (error) => {
+        //console.error("Error al cargar productos: ", error);
+        alert("error al cargar productos: "+ error);
+      }
+    );
+  }
+
+  nuevoProducto(): void{
+    
+  }
+  editarProducto(producto: any){
+
+  }
+
+  eliminarProducto(id: number) {
+
+  }
 
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaderResponse } from '@angular/common/http';
+import { HttpClient, HttpHeaderResponse, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
+import { Producto } from '../interfaces/producto';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,26 @@ export class ProductosService {
 
   constructor(private http: HttpClient) {}
 
-  getProductos(): Observable<any>{
-    return this.http.get(`${this.apiUrl}/`);
+  private getAuthHeaders(): HttpHeaders {
+    const username = 'user';
+    const password = 'a6c12ece-6e87-4ad2-8c50-3a3ab3418c93';
+    const basicAuth = 'Basic ' + btoa(`${username}:${password}`);
+    return new HttpHeaders({ Authorization: basicAuth });
+  }
+
+  getProductos(): Observable<Producto[]>{
+    return this.http.get<Producto[]>(`${this.apiUrl}/list`, { headers: this.getAuthHeaders() });
+  }
+
+  crearProducto(producto: Producto): Observable<string> {
+    return this.http.post<string>(`${this.apiUrl}/create`, producto, { headers: this.getAuthHeaders() });
+  }
+
+  actualizarProducto(id: number, producto: Producto): Observable<string> {
+    return this.http.put<string>(`${this.apiUrl}/update/${id}`, producto, { headers: this.getAuthHeaders() });
+  }
+
+  eliminarProducto(id: number): Observable<string> {
+    return this.http.delete<string>(`${this.apiUrl}/delete/${id}`, { headers: this.getAuthHeaders() });
   }
 }
