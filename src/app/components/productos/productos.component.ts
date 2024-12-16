dbaa5a259eff5b6558ab5c45d949b910376d70a6
Italo Espinoza <Italoespinoza1529@gmail.com>
2024-12-16 01:42:06 -0500

    add product and login - complete

--- src/app/components/productos/productos.component.ts
+++ src/app/components/productos/productos.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductosService } from '../../services/productos.service';
 import { Producto } from '../../interfaces/producto';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule  } from '@angular/forms';
 import Swal from 'sweetalert2';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatTableDataSource } from '@angular/material/table'; 
 import { MatPaginator } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+//import { BrowserAnimationsModule  } from '@angular/platform-browser/animations';
 
 @Component({
   standalone: true,
@@ -20,60 +24,234 @@ import { MatPaginator } from '@angular/material/paginator';
     MatPaginator, 
     MatButtonModule, 
     MatIconModule,
-    MatDialogModule
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    //BrowserAnimationsModule 
   ],
   templateUrl: './productos.component.html',
   styleUrl: './productos.component.scss'
 })
-export class ProductosComponent implements OnInit, AfterViewInit {
+export class ProductosComponent implements OnInit {
   productos: Producto[] = [];
   displayedColumns: string[] = ['id', 'nombre', 'descripcion', 'precio', 'stock', 'acciones'];
-  dataSource: MatTableDataSource<Producto> = new MatTableDataSource<Producto>([]);
+  paginacion: Producto[] = [];
+  numeroPagina = 5;
+  numeroActual = 0;
+  totalProductos = 0;
+  productoForm: FormGroup;
+  isDialogVisible: boolean = false;
+  producto?: Producto;
+  titulo?: string;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild('dialogTemplate') dialogTemplate!: TemplateRef<any>;
 
   constructor(
     private productosService: ProductosService,
     private router: Router,
-    private dialog: MatDialogModule
-  ) { }
+    private dialog: MatDialog,
+    private fb: FormBuilder
+  ) {
+    this.productoForm = this.fb.group({
+      idProducto: [null],
+      nombre: ['', Validators.required],
+      descripcion: ['', Validators.required],
+      precio: ['', Validators.required],
+      stock: ['', Validators.required]
+    });
+  }
 
   ngOnInit(): void {
     this.getProductos();
   }
 
-  ngAfterViewInit(): void {
-    if(this.paginator){
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.paginator.firstPage(); 
-    }else {
-      //console.error("Paginator no está inicializado.");
-    }
-  }
-
   getProductos(): void {
     this.productosService.getProductos().subscribe(
       (data) => {
-        //console.log("Productos obtenidos: ", data);
         this.productos = data;
-        this.dataSource.data = this.productos;
+        //console.log("Productos: ", this.productos);
+        this.totalProductos = this.productos.length;
+        this.productosPaginados();
+        //this.paginator.firstPage();
+        //this.dataSource.data = data;//this.productos;
       },
       (error) => {
         //console.error("Error al cargar productos: ", error);
-        alert("error al cargar productos: "+ error);
+        //alert("error al cargar productos: "+ error);
+        //const mensaje = error.error.message
+          Swal.fire({
+            allowOutsideClick: true,
+            title: 'Failed',
+            text: 'Error en el servidor',
+            icon: 'warning',
+            confirmButtonText: 'Aceptar'
+        })
       }
     );
   }
 
-  nuevoProducto(): void{
+  productosPaginados(): void {
+    const inicio = this.numeroActual * this.numeroPagina;
+    const final = inicio + this.numeroPagina;
+    this.paginacion = this.productos.slice(inicio, final);
     
   }
-  editarProducto(producto: any){
 
+  openDialog() {
+    //this.isDialogVisible =true;
+    this.producto = undefined;
+    this.titulo = 'Nuevo Producto';
+    this.dialog.open(this.dialogTemplate, {
+      width: '400px'
+    });
+  }
+
+  saveProducto(): void {
+    if(this.productoForm.valid){
+      const productoData: Producto = this.productoForm.value;
+      productoData.idCategoria = 1; 
+      
+      if (!this.producto?.idProducto) {
+        //console.log("producto:" + JSON.stringify(productoData));
+        this.productosService.crearProducto(productoData).subscribe(
+          (response) => {
+            const mensaje = response.message
+            Swal.fire({
+              allowOutsideClick: true,
+              title: 'Success',
+              text: mensaje,
+              icon: 'success',
+              confirmButtonText: 'regresar'
+            }).then((click) => {
+              if(click.isConfirmed){
+                this.getProductos();
+                this.closeDialog();
+              }
+            });
+          },
+          (error) =>{
+            //console.log(error);
+            const mensaje = error.error.message
+            Swal.fire({
+              allowOutsideClick: true,
+              title: 'Failed',
+              text: mensaje,
+              icon: 'warning',
+              confirmButtonText: 'Aceptar'
+            })
+          }
+      );
+      }else {
+        this.productosService.actualizarProducto(this.producto.idProducto, productoData).subscribe(
+          (response) => {
+            const mensaje = response.message;
+            Swal.fire({
+              allowOutsideClick: true,
+              title: 'Success',
+              text: mensaje,
+              icon: 'success',
+              confirmButtonText: 'Regresar'
+            }).then((click) => {
+              if(click.isConfirmed){
+                this.getProductos();
+                this.closeDialog();
+              }
+            });
+          },
+          (error) => {
+            const mensaje = error.error?.message;
+            Swal.fire({
+              allowOutsideClick: true,
+              title: 'Failed',
+              text: mensaje,
+              icon: 'warning',
+              confirmButtonText: 'Aceptar'
+            });
+          }
+        )
+      }
+    }else{
+
+    }
+  }
+
+  editarProducto(producto: Producto){
+    this.producto = producto;
+    console.log(this.producto);
+    
+    this.productoForm.patchValue({
+      idProducto: producto.idProducto,
+      nombre: producto.nombre,
+      descripcion: producto.descripcion,
+      precio: producto.precio,
+      stock: producto.stock
+    });
+    this.titulo = 'Editar Producto';
+    //this.isDialogVisible = true;
+    this.dialog.open(this.dialogTemplate, {
+      width: '400px'
+    });
   }
 
   eliminarProducto(id: number) {
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: "No podrás revertir esta acción",
+      icon: 'error',
+      showCancelButton: true,
+      confirmButtonColor: '#d33',
+      cancelButtonColor: '#3085d6',
+      confirmButtonText: 'Sí, eliminar'
+    }).then((click) => {
+      if(click.isConfirmed){
+        this.productosService.eliminarProducto(id).subscribe(
+          (response) => {
+            const mensaje = response.message;
+            Swal.fire({
+              title: 'Eliminado',
+              text: mensaje,
+              icon: 'success',
+              confirmButtonText: 'Aceptar'
+          }).then(() => {
+              this.getProductos();
+          });
+          },
+          (error) => {
+            const mensaje = error.error?.message;
+            Swal.fire({
+                title: 'Error',
+                text: mensaje,
+                icon: 'error',
+                confirmButtonText: 'Aceptar'
+            });
+        }
+        );
+      }
 
+    });
+  }
+
+  abrirCarrito(){
+    //alert("");
+  }
+
+  logout(){
+    localStorage.removeItem('username');
+    localStorage.removeItem('password');
+    this.router.navigate(['/login']);
+  }
+
+  closeDialog() {
+    this.productoForm.reset();
+    //this.isDialogVisible = false;
+    this.dialog.closeAll();
+  }
+
+  onPage(event: any): void{
+    this.numeroActual = event.pageIndex;
+    this.numeroPagina = event.pageSize;
+    this.productosPaginados();
   }
 
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaderResponse, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
 import { Producto } from '../interfaces/producto';
@@ -12,26 +12,26 @@ export class ProductosService {
 
   constructor(private http: HttpClient) {}
 
-  private getAuthHeaders(): HttpHeaders {
+  private getAutenticacion(): HttpHeaders {
     const username = 'user';
-    const password = 'a6c12ece-6e87-4ad2-8c50-3a3ab3418c93';
-    const basicAuth = 'Basic ' + btoa(`${username}:${password}`);
-    return new HttpHeaders({ Authorization: basicAuth });
+    const password = 'fca86b43-f07b-4d0b-8ec3-e4b4f16ba6eb';
+    const basica = 'Basic ' + btoa(`${username}:${password}`);
+    return new HttpHeaders({ Authorization: basica });
   }
 
   getProductos(): Observable<Producto[]>{
-    return this.http.get<Producto[]>(`${this.apiUrl}/list`, { headers: this.getAuthHeaders() });
+    return this.http.get<Producto[]>(`${this.apiUrl}/list`, { headers: this.getAutenticacion() });
   }
 
-  crearProducto(producto: Producto): Observable<string> {
-    return this.http.post<string>(`${this.apiUrl}/create`, producto, { headers: this.getAuthHeaders() });
+  crearProducto(producto: Producto): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/create`, producto, { headers: this.getAutenticacion() });
   }
 
-  actualizarProducto(id: number, producto: Producto): Observable<string> {
-    return this.http.put<string>(`${this.apiUrl}/update/${id}`, producto, { headers: this.getAuthHeaders() });
+  actualizarProducto(id: number, producto: Producto): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/update/${id}`, producto, { headers: this.getAutenticacion() });
   }
 
-  eliminarProducto(id: number): Observable<string> {
-    return this.http.delete<string>(`${this.apiUrl}/delete/${id}`, { headers: this.getAuthHeaders() });
+  eliminarProducto(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/delete/${id}`, { headers: this.getAutenticacion() });
   }
 }
